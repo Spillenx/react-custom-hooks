716ade0096c0a424101293eafb73fcd4e15111b5
SpillenX <spillenx@gmail.com>
2021-03-25 23:37:30 +0100

    fetch hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useFetch } from './useFetch'
+
+function App({login}) {
+  const { loading, data, error } = useFetch(`https://api.github.com/users/${login}`)
+  if(loading) return <h1>Loading...</h1>
+  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  return(
+    <div>
+      <img src={data.avatar_url} alt={data.login} />
+      <div>
+        <h1>{data.login}</h1>
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App login="spillenx" />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
